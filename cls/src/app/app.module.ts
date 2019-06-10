64147827f45b87883b71a1f2e5be2f9214c28944
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-10 16:06:09 +0100

    add hash to routes

--- cls/src/app/app.module.ts
+++ cls/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarService } from './services/navbar.service';
 import { FooterService } from './services/footer.service';
 import { TestComponent } from './test/test.component';
 import {routes} from './app-routing.module';
+import {HashLocationStrategy, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 
 @NgModule({
@@ -31,7 +32,9 @@ import {routes} from './app-routing.module';
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [NavbarService, FooterService],
+  providers: [NavbarService, FooterService,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
