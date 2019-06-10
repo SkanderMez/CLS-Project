5f36ddd7a747aad533661dbafe32df3dbeb30474
hamzadaoues <37672255+hamzadaoues@users.noreply.github.com>
2019-06-10 18:08:23 +0100

    about us
    pas finis

---
+++ cls/src/app/services/about-us.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+//import  file from '../../assets/about_us.json' ;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AboutUsService {
+
+  constructor() {
+        //console.log(file);
+  }
+}
