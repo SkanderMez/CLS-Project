05d1a58be6b11109e9c7baf31bb6db96d00c6275
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-11 13:11:39 +0100

    move data to json

--- cls/src/app/services/contact.service.ts
+++ cls/src/app/services/contact.service.ts
@@ -1,24 +1,19 @@
 import { Injectable } from '@angular/core';
 import {ContactModel} from '../components/_models/contact.model';
-import {Observable} from 'rxjs';
+// @ts-ignore
+import file from '../../assets/json/contact.json' ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  contact: ContactModel = {
-    email: 'abc@gmail.com',
-    phone: '1234567890',
-    address1: 'address1',
-    address2: 'address2'
-  };
 
   constructor() { }
 
   getContact(): Promise<ContactModel> {
     return new Promise<ContactModel>(resolve => {
-      resolve (this.contact);
+      resolve (file.contact);
     });
   }
 }
