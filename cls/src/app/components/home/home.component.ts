66122f34245cfd73e48a13c66b179e846f2d5817
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-12 09:28:09 +0100

    add dynamic about us and client infos

--- cls/src/app/components/home/home.component.ts
+++ cls/src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ContactModel} from '../_models/contact.model';
 import {ContactService} from '../../services/contact.service';
+import {AboutUsService} from '../../services/about-us.service';
+import {ClientService} from '../../services/client.service';
+import {ClientModel} from '../_models/client.model';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +13,24 @@ import {ContactService} from '../../services/contact.service';
 export class HomeComponent implements OnInit {
 
   contact: ContactModel;
-  constructor(private contactService: ContactService) { }
+  aboutUs: any;
+  clients: ClientModel[];
+  constructor(private contactService: ContactService,
+              private clientService: ClientService,
+              private aboutUsService: AboutUsService) { }
 
   ngOnInit() {
     this.contactService.getContact()
       .then(contact => {
         this.contact = contact;
+      });
+    this.aboutUsService.getAboutus()
+      .then(aboutUs => {
+        this.aboutUs = aboutUs;
+      });
+    this.clientService.getClients()
+      .then(clients => {
+        this.clients = clients;
       });
   }
 
